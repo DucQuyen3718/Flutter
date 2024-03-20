403551e2ab5b0c7d9d7059df96ce862d658415fd
DucQuyen3718 <phamduc3718@gmail.com>
2024-03-20 19:12:22 +0700

    chua hien thi duoc bai viet

---
+++ news_app/lib/article_model.dart
@@ -0,0 +1,14 @@
+// import 'package:flutter/material.dart';
+
+class Article {
+  final String title;
+  final String urlToImage;
+  final String description; // Thêm trường mô tả
+
+  Article({
+    required this.title,
+    required this.urlToImage,
+    required this.description,
+  });
+}
+
